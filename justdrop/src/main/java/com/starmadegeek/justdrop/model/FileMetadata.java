4c6fad9a0c4b58085f2943c89d512b3d4c1831be
starmadegeek <nivyanth.developer@gmail.com>
2025-07-06 19:47:12 +0530

    repository and pojo

--- justdrop/src/main/java/com/starmadegeek/justdrop/model/FileMetadata.java
+++ justdrop/src/main/java/com/starmadegeek/justdrop/model/FileMetadata.java
@@ -1,4 +1,77 @@
 package com.starmadegeek.justdrop.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "file_metadata")
 public class FileMetadata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String originalFilename;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(nullable = false)
+    private LocalDateTime uploadTime;
+
+    public FileMetadata() {}
+
+    public FileMetadata(String filename, String originalFilename, String contentType,
+                        Long size, String filePath, LocalDateTime uploadTime) {
+        this.filename = filename;
+        this.originalFilename = originalFilename;
+        this.contentType = contentType;
+        this.size = size;
+        this.filePath = filePath;
+        this.uploadTime = uploadTime;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFilename() { return filename; }
+    public void setFilename(String filename) { this.filename = filename; }
+
+    public String getOriginalFilename() { return originalFilename; }
+    public void setOriginalFilename(String originalFilename) { this.originalFilename = originalFilename; }
+
+    public String getContentType() { return contentType; }
+    public void setContentType(String contentType) { this.contentType = contentType; }
+
+    public Long getSize() { return size; }
+    public void setSize(Long size) { this.size = size; }
+
+    public String getFilePath() { return filePath; }
+    public void setFilePath(String filePath) { this.filePath = filePath; }
+
+    public LocalDateTime getUploadTime() { return uploadTime; }
+    public void setUploadTime(LocalDateTime uploadTime) { this.uploadTime = uploadTime; }
+
+    @Override
+    public String toString() {
+        return "FileMetadata{" +
+                "id=" + id +
+                ", filename='" + filename + '\'' +
+                ", originalFilename='" + originalFilename + '\'' +
+                ", contentType='" + contentType + '\'' +
+                ", size=" + size +
+                ", filePath='" + filePath + '\'' +
+                ", uploadTime=" + uploadTime +
+                '}';
+    }
 }
--- justdrop/src/main/java/com/starmadegeek/justdrop/repository/FileMetadataRepository.java
+++ justdrop/src/main/java/com/starmadegeek/justdrop/repository/FileMetadataRepository.java
@@ -1,4 +1,9 @@
 package com.starmadegeek.justdrop.repository;
 
-public class FileMetadataRepository {
+import com.starmadegeek.justdrop.model.FileMetadata;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
 }
