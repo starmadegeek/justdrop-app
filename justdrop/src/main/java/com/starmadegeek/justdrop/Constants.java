18edfdfb92c9d72fbb174226559ca482f1d9f28b
starmadegeek <nivyanth.developer@gmail.com>
2025-07-06 20:15:22 +0530

    fix

--- justdrop/src/main/java/com/starmadegeek/justdrop/Constants.java
+++ justdrop/src/main/java/com/starmadegeek/justdrop/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 
     public static final String FILE_METADATA_NOT_FOUND = "File metadata not found with id: '{}'";
 
-    private ErrorMessages() {
+    private Constants() {
 
     }
 }
